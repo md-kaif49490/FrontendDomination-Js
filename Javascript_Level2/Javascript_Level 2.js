5d0c3348f5d1a837bb6cec91084dfd56886331db
Md Kaiful Haque <142306315+md-kaif49490@users.noreply.github.com>
2025-01-09 22:09:13 +0530

    Update Javascript_Level 2.js

--- Javascript_Level2/Javascript_Level 2.js
+++ Javascript_Level2/Javascript_Level 2.js
@@ -9,11 +9,11 @@ const num = 12; //const change nahi hota hai
 
 //compiler and interpreter
 
-//compiler and interpreter translator hai jo bhi ham code likhte use machine code mein covert karta hai 
+//compiler and interpreter translator hai jo bhi ham code likhte usey machine code mein convert karta hai 
 //interpreter byte code mein covert hota hai byte code basicaly hardware ke kam specific hota hai aur machine code hardware ke specif hota hai
 //isilye compiler thora late run hota hai but execute fast hota hai  aur interpreter thora fast run  hota hai  but execute late hota hai
 
-//isiliye google ne - jit compiler launch kiya ye interpreter ki suruwati takat aur compiler ki executation takat ka mixture hai
+//isiliye google ne - jit compiler launch kiya ye interpreter ki suruwati takat(Running Capability) aur compiler ki executation Capability ka mixture hai
 
 
 //Window - browser ka part hai jo ki ham js mein use karte hain
